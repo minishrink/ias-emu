b7ed7d17cbde3e00a44ae70dbdc8946f74d0a1f5
minishrink <minishrink@users.noreply.github.com>
2025-03-25 19:01:09 +0000

    Use bitmasks to wrangle 64-bits into 40 and 20
    Signed-off-by: minishrink <minishrink@users.noreply.github.com>

---
+++ machine/_bitmasks.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define LAST_N_BITS(n) (((uint64_t)1 << n)-1)
+
+#define MASK_BYTE   (LAST_N_BITS(8))
+#define MASK_HIBYTE (LAST_N_BITS(8) << 56)
+#define MASK_ADDR   (LAST_N_BITS(12))
+#define MASK_LO20  (LAST_N_BITS(20))
+#define MASK_HI20  (LAST_N_BITS(20) << 20)
+#define MASK_LO40  (LAST_N_BITS(40))
+
+/** longword assumed to be 64 bits **/
+
+#define HIBYTE(_longword) (_longword & MASK_HIBYTE)
+#define LO20(_longword)   (_longword & MASK_LO20)
+#define HI20(_longword)   (_longword & MASK_HI20)
+#define LO40(_longword)   (_longword & MASK_LO40)
