116438c324fc5ef32137a53e5d5ff03fd67bf7fe
minishrink <minishrink@users.noreply.github.com>
2025-03-31 22:29:01 +0100

    Add test asserts for different bit widths
    Signed-off-by: minishrink <minishrink@users.noreply.github.com>

--- test/_assert.c
+++ test/_assert.c
@@ -13,12 +13,12 @@ uint32_t inline _assert(uint32_t _bool) {
   }
   else {
     PRINT_FAIL();
-    printf(" -- %d", _bool);
+    printf(" -- %x", _bool);
   }
   ++test_count;
   return _bool;
 }
-void inline _assert_streq(const char* x, const char* y) {
+void inline _assert_streq(const char *x, const char *y) {
   if (strcmp(x,y) == 0) {
     PRINT_PASS();
     ++pass_count;
@@ -29,8 +29,47 @@ void inline _assert_streq(const char* x, const char* y) {
   }
   ++test_count;
 }
+void inline _assert_uinteq(uint64_t x, uint64_t y) {
+  if (x == y) {
+    PRINT_PASS();
+    ++pass_count;
+  }
+  else {
+    PRINT_FAIL();
+    printf(" -- %10lx != %10lx", x,y);
+  }
+  ++test_count;
+}
+void inline _assert_uint32(uint32_t x, uint32_t y) {
+  if (x == y) {
+    PRINT_PASS();
+    ++pass_count;
+  }
+  else {
+    PRINT_FAIL();
+    printf(" -- %10x != %10x", x,y);
+  }
+  ++test_count;
+}
+void inline _assert_uint8(uint8_t x, uint8_t y) {
+  if (x==y) {
+    PRINT_PASS();
+    ++pass_count;
+  }
+  else {
+    PRINT_FAIL();
+    printf(" -- %10x != %10x", x, y);
+  }
+  ++test_count;
+}
 void inline _print_test_count(void) {
   printf("\n\033[1mPassed %3u / %3u\033[0m", pass_count, test_count);
 }
-
-#define END_TEST() _print_test_count()
+void inline _new_test(const char *_name) {
+  if (test_count > 0) {
+    _print_test_count();
+    test_count = 0u;
+    pass_count = 0u;
+  }
+  printf("\n\n\033[1m--- %s ---\033[0m", _name);
+}
--- test/_assert.h
+++ test/_assert.h
@@ -2,14 +2,24 @@
 #include <stdint.h>
 
 #define PRINT_PASS() (printf("\n\033[92mPASSED\033[0m"))
-
 #define PRINT_FAIL() (printf("\n\033[91mFAILED\033[0m"))
+
 uint32_t _assert(uint32_t _bool);
-void _assert_streq(const char* x, const char* y);
+void     _assert_streq(const char *x, const char *y);
+void     _assert_uinteq(uint64_t x, uint64_t y);
+void     _assert_uint32(uint32_t x, uint32_t y);
+void     _assert_uint8(uint8_t x, uint8_t y);
+void     _print_test_count(void);
+void     _new_test(const char *name);
+
 #define ASSERT(_b) (_assert(_b))
 #define ASSERT_STREQ(_x, _y) (_assert_streq(_x,_y))
-void _print_test_count(void);
-#define END_TEST() _print_test_count()
+#define ASSERT_UINTEQ(_x, _y) (_assert_uinteq(_x,_y))
+#define ASSERT_UINT8(_x, _y) (_assert_uint8(_x,_y))
+#define ASSERT_UINT32(_x, _y) (_assert_uint32(_x,_y))
+
+#define START_TEST(_name) _new_test(_name)
+#define END_TEST() (_print_test_count())
 
 extern uint16_t pass_count;
 extern uint16_t test_count;
